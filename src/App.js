ec3a4bc646f6cf042abece9882075727617a0ba4
micha-code <mi.davidova@gmail.com>
2020-11-24 13:23:14 +0100

    hook ref viewer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import { Cartesian3, createWorldTerrain } from 'cesium';
+import React, { useState, useRef, useEffect } from 'react';
+import { Viewer, Entity } from 'resium';
 
-const terrainProvider = createWorldTerrain();
-const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
-
-const App = () => (
-  <Viewer full terrainProvider={terrainProvider}>
-    <Entity position={position} name="Tokyo">
-      <PointGraphics pixelSize={10} />
-      <EntityDescription>
-        <h1>Hello, world.</h1>
-        <p>JSX is available here!</p>
-      </EntityDescription>
-    </Entity>
-  </Viewer>
-);
-
+function App() {
+  const viewer = useRef(null);
+  return <Viewer ref={viewer} />;
+}
 export default App;
 
+// import React from 'react';
+// import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
+// import { Cartesian3, createWorldTerrain } from 'cesium';
+
+// const terrainProvider = createWorldTerrain();
+// const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
+
+// const App = () => (
+//   <Viewer full terrainProvider={terrainProvider}>
+//     <Entity position={position} name="Tokyo">
+//       <PointGraphics pixelSize={10} />
+//       <EntityDescription>
+//         <h1>Hello, world.</h1>
+//         <p>JSX is available here!</p>
+//       </EntityDescription>
+//     </Entity>
+//   </Viewer>
+// );
+
+// export default App;
