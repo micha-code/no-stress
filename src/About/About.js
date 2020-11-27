15e26ce3691dd53d30087e6596004ea0841736c5
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-11-27 17:28:35 +0100

    Routing

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <h1>
+      This will be the page about us
+    </h1>
+  )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './About/About.js';
+import AddNew from './Add-new/Add-new.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/add-new">Add new</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/add-new">
+          <AddNew />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </main>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root'),
 );
 
