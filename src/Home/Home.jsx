47c778c13f6123ebcab2663fe941af5d26fa335c
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-11-28 09:57:56 +0100

    oprava chyby

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Viewer, Entity } from 'resium';
-import { Ion } from "cesium";
-
+import { Color, Ion } from 'cesium';
 
 function Home() {
-  Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YzJhMjU1YS0zMDY5LTRkN2QtOTMzMS1lY2FkZWYwYTUwYzkiLCJpZCI6Mzc3NTksImlhdCI6MTYwNTU1MjQ0OX0.bT7I-PurpKWvzE-xack9rB9uFLdEVameSvWT6v159WQ";
+  Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YzJhMjU1YS0zMDY5LTRkN2QtOTMzMS1lY2FkZWYwYTUwYzkiLCJpZCI6Mzc3NTksImlhdCI6MTYwNTU1MjQ0OX0.bT7I-PurpKWvzE-xack9rB9uFLdEVameSvWT6v159WQ';
 
   const viewer = useRef(null);
-  return <Viewer ref={viewer} Scene backgroundColor={Color.CORNFLOWERBLUE}  />;
+  return <Viewer ref={viewer} Scene backgroundColor={Color.CORNFLOWERBLUE} />;
 }
 export default Home;
 
