d429a426e6daa785cf5f9dfb8b0173da43e5a980
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-11-28 17:44:02 +0100

    stylovani btn countries a categories

--- src/FilterCategories/FilterCategories.jsx
+++ src/FilterCategories/FilterCategories.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './FilterCategories.css';
 
 const FilterCategories = () => {
-  return <h2>Filter Categories</h2>;
+  return (
+    <>
+      <div className="container">
+        <h2 className="info-category">Filter Categories</h2>
+      </div>
+    </>
+  );
 };
 
 export default FilterCategories;
