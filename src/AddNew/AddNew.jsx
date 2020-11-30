3b595cd2dd0a7d092380cdd050cd5751b3f168fd
micha-code <mi.davidova@gmail.com>
2020-11-30 09:03:58 +0100

    style of addnew

--- src/AddNew/AddNew.jsx
+++ src/AddNew/AddNew.jsx
@@ -75,7 +75,7 @@ const AddNew = () => {
         your time to shine! Share your tips and recommendations with others!
       </p>
 
-      <form ref={form} onSubmit={handleSubmit}>
+      <form className="form" ref={form} onSubmit={handleSubmit}>
         <div>
           <label>
             ADD NAME <br></br>
