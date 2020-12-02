f18169d8cf05346cf250ceed5cb9463134af3022
ivze <ivonaz@centrum.cz>
2020-12-02 19:33:20 +0100

    select category

--- src/FilterCategories/FilterCategories.jsx
+++ src/FilterCategories/FilterCategories.jsx
@@ -56,7 +56,7 @@ const FilterCategories = ({ category, setCategory }) => {
             />
           </>
         ) : (
-          'Choose a category'
+          'Select Category'
         )}
       </div>
       {rollOut && (
