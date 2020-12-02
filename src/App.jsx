0111aa23d4da41f57d6923b53508eba2675bfb43
micha-code <mi.davidova@gmail.com>
2020-12-02 10:33:12 +0100

    changed menu CSS

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,36 @@ const App = () => {
   const [open, setOpen] = useState(false);
   return (
     <Router>
-      <nav className="navbar">
-        <img
-          className="LogoImg"
-          src="images/logo-menu.svg"
-          alt="Logo no-stress"
-        />
-        <button
-          className={open ? 'hamburger hamburger_open' : 'hamburger'}
-          aria-label="menu"
-          onClick={() => setOpen(!open)}
-        >
-          <img className="LogoMenu" src="images/menu.svg" alt="Menu" />
-        </button>
-        {open && (
-          <ul className="menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/addNew">Add new</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        )}
+      <nav className="topbar_container">
+        <div className="SiteLogo">
+          <img
+            className="LogoImg"
+            src="images/logo-menu.svg"
+            alt="Logo no-stress"
+          />
+        </div>
+        <div className="navbar">
+          <button
+            className={open ? 'hamburger hamburger_open' : 'hamburger'}
+            aria-label="menu"
+            onClick={() => setOpen(!open)}
+          >
+            <img className="LogoMenu" src="images/menu.svg" alt="Menu" />
+          </button>
+          {open && (
+            <ul className="menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/addNew">Add new</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          )}
+        </div>
       </nav>
       <main>
         <Switch>
